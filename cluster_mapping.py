3b5660dda2b7332abf5cdd26e70f5b3d8fc86877
Yahitha7 <yahithakasireddy03@gmail.com>
2026-08-24 12:36:46 +0530

    Add cluster mapping configuration
    This file contains a mapping of clusters with their respective locations, types, target storage, regions, countries, and cities.

---
+++ cluster_mapping.py
@@ -0,0 +1,222 @@
+CLUSTER_MAPPING = {
+
+    "RBKAMS01": {
+        "Location": "Amstelveen, Netherlands",
+        "Type": "Rubrik",
+        "Target Storage": "RBKAMS01",
+        "Region": "EU",
+        "Country": "Netherlands",
+        "City": "Amstelveen"
+    },
+
+    "RBKATH01": {
+        "Location": "Athens, Greece",
+        "Type": "Rubrik",
+        "Target Storage": "RBKATH01",
+        "Region": "EU",
+        "Country": "Greece",
+        "City": "Athens"
+    },
+
+    "RBKAUR01": {
+        "Location": "East Aurora, NY, USA",
+        "Type": "Rubrik",
+        "Target Storage": "RBKAUR01",
+        "Region": "NA",
+        "Country": "USA",
+        "City": "East Aurora"
+    },
+
+    "RBKBNK01": {
+        "Location": "Bangkok, Thailand",
+        "Type": "Rubrik",
+        "Target Storage": "RBKBNK01",
+        "Region": "APAC",
+        "Country": "Thailand",
+        "City": "Bangkok"
+    },
+
+    "RBKCAC01": {
+        "Location": "Shaanxi, China",
+        "Type": "Rubrik",
+        "Target Storage": "RBKCAC01",
+        "Region": "APAC",
+        "Country": "China",
+        "City": "Shaanxi"
+    },
+
+    "RBKCKR01": {
+        "Location": "Cikarang, Bekasi, West Java, Indonesia",
+        "Type": "Rubrik",
+        "Target Storage": "RBKCKR01",
+        "Region": "APAC",
+        "Country": "Indonesia",
+        "City": "Cikarang"
+    },
+
+    "RBKCKS01": {
+        "Location": "Jakarta, Indonesia",
+        "Type": "Rubrik",
+        "Target Storage": "RBKCKS01",
+        "Region": "APAC",
+        "Country": "Indonesia",
+        "City": "Jakarta"
+    },
+
+    "RBKESC01": {
+        "Location": "LIBRAMINETO NOROESTE KM 27, Centro, 66050 Cd Gral Escobedo, N.L., Mexico",
+        "Type": "Rubrik",
+        "Target Storage": "RBKESC01",
+        "Region": "LATAM",
+        "Country": "Mexico",
+        "City": "Escobedo"
+    },
+
+    "RBKEST01": {
+        "Location": "El Segundo, CA, USA",
+        "Type": "Rubrik",
+        "Target Storage": "RBKEST01",
+        "Region": "NA",
+        "Country": "USA",
+        "City": "El Segundo"
+    },
+
+    "RBKGUA01": {
+        "Location": "Guanyao, Shanzhou District, Sanmenxia, Henan, China",
+        "Type": "Rubrik",
+        "Target Storage": "RBKGUA01",
+        "Region": "APAC",
+        "Country": "China",
+        "City": "Guanyao"
+    },
+
+    "RBKHEN01": {
+        "Location": "Shenzhen, Guangdong Province, China",
+        "Type": "Rubrik",
+        "Target Storage": "RBKHEN01",
+        "Region": "APAC",
+        "Country": "China",
+        "City": "Shenzhen"
+    },
+
+    "RBKKLN01": {
+        "Location": "Kowloon, Hong Kong",
+        "Type": "Rubrik",
+        "Target Storage": "RBKKLN01",
+        "Region": "APAC",
+        "Country": "Hong Kong",
+        "City": "Kowloon"
+    },
+
+    "RBKLAS01": {
+        "Location": "Las Vegas, USA",
+        "Type": "Rubrik",
+        "Target Storage": "RBKLAS01",
+        "Region": "NA",
+        "Country": "USA",
+        "City": "Las Vegas"
+    },
+
+    "RBKLON01": {
+        "Location": "Slough Trading Estate, UK",
+        "Type": "Rubrik",
+        "Target Storage": "RBKLON01",
+        "Region": "EU",
+        "Country": "UK",
+        "City": "London"
+    },
+
+    "RBKMEXC01": {
+        "Location": "Mexico City, CDMX, Mexico",
+        "Type": "Rubrik",
+        "Target Storage": "RBKMEXC01",
+        "Region": "LATAM",
+        "Country": "Mexico",
+        "City": "Mexico City"
+    },
+
+    "RBKMID01": {
+        "Location": "Middleton, WI, USA",
+        "Type": "Rubrik",
+        "Target Storage": "RBKMID01",
+        "Region": "NA",
+        "Country": "USA",
+        "City": "Middleton"
+    },
+
+    "RBKMIL01": {
+        "Location": "Milan, Italy",
+        "Type": "Rubrik",
+        "Target Storage": "RBKMIL01",
+        "Region": "EU",
+        "Country": "Italy",
+        "City": "Milan"
+    },
+
+    "RBKMIS01": {
+        "Location": "Mississauga, Canada",
+        "Type": "Rubrik",
+        "Target Storage": "RBKMIS01",
+        "Region": "NA",
+        "Country": "Canada",
+        "City": "Mississauga"
+    },
+
+    "RBKMON01": {
+        "Location": "4505 Rue Hickmore, Saint-Laurent, QC, Canada",
+        "Type": "Rubrik",
+        "Target Storage": "RBKMON01",
+        "Region": "NA",
+        "Country": "Canada",
+        "City": "Saint Laurent"
+    },
+
+    "RBKPEN01": {
+        "Location": "Penang, Malaysia",
+        "Type": "Rubrik",
+        "Target Storage": "RBKPEN01",
+        "Region": "APAC",
+        "Country": "Malaysia",
+        "City": "Penang"
+    },
+
+    "RBKPER01": {
+        "Location": "Perai, Penang, Malaysia",
+        "Type": "Rubrik",
+        "Target Storage": "RBKPER01",
+        "Region": "APAC",
+        "Country": "Malaysia",
+        "City": "Perai"
+    },
+
+    "RBKSAN01": {
+        "Location": "Santiago, Santiago Metropolitan Region",
+        "Type": "Rubrik",
+        "Target Storage": "RBKSAN01",
+        "Region": "LATAM",
+        "Country": "Chile",
+        "City": "Santiago"
+    },
+
+    "RBKSBDC01": {
+        "Location": "San Bernardino, CA, USA",
+        "Type": "Rubrik",
+        "Target Storage": "RBKSBDC01",
+        "Region": "NA",
+        "Country": "USA",
+        "City": "San Bernardino"
+    },
+
+    "RBKYAN01": {
+        "Location": "Yunnan, China",
+        "Type": "Rubrik",
+        "Target Storage": "RBKYAN01",
+        "Region": "APAC",
+        "Country": "China",
+        "City": "Yunan"
+    }
+}
+
+
+TH_HIGH = 0.85
+TH_LOW = 0.30
