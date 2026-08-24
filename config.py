a5028d224dfd230dd7721a2bbdb449216f226082
Yahitha7 <yahithakasireddy03@gmail.com>
2026-08-24 12:38:51 +0530

    Add configuration settings for client API

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+CLIENT_ID = "client|8220170e-1043-4a74-99d0-9453157d41bb"
+CLIENT_SECRET = "IuvqCLxfRO9FCptwWo0Z6_YGiT-5xiSYoGBxCrVT8V0vmgJILGiJZZhIgzDLYtGC"
+
+TOKEN_URL = "https://mattel.my.rubrik.com/api/client_token"
+
+GRAPHQL_URL = "https://mattel.my.rubrik.com/api/graphql"
